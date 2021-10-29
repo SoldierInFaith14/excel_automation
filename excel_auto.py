e1921499fb532f3fdd8f37ba542b0d3c1d73f0d8
SoldierInFaith14 <neil.podoll@my.smsu.edu>
2021-10-29 01:40:58 -0500

    format date

--- excel_auto.py
+++ excel_auto.py
@@ -7,11 +7,30 @@ from openpyxl.styles import Font, colors
 
 shipping_costs = []
 customer_row_nums = []
-highlight = PatternFill(patternType='solid', fgColor=colors.Color(rgb='00FF55'))
+highlight_green = PatternFill(patternType='solid', fgColor=colors.Color(rgb='00FF55'))
+highlight_blue = PatternFill(patternType='solid', fgColor=colors.Color(rgb='0dacfa'))
 
-# Get each unique order and their shipping costs.
-def getCustomerInfo(ws):
+# Format date and get order shipping costs.
+def formatOrderInfo(ws):
     for row in range(2, ws.max_row):
+        try:
+            # Format date to mm/dd/yyyy
+            for col in range(4, 5):
+                char = get_column_letter(col)
+                if ws[char + str(row)].value != None:
+                    cell_date = (ws[char + str(row)].value[0:10:1])
+                    year, month, day = cell_date.split('-')
+                    cell_date = month + "/" + day + "/" + year
+                    ws[char + str(row)].value = None
+                    ws[char + str(row)].value = cell_date
+                    ws[char + str(row)].fill = highlight_blue
+                else:
+                    continue
+
+        except TypeError:
+            pass
+
+        # Getting shipping costs
         for col in range(10, 11):
             char = get_column_letter(col)
             if ws[char + str(row)].value != None:
@@ -41,15 +60,16 @@ def insertRows(ws):
 
         finalRow = ws.append({'A': ws['A' + str(ws.max_row)].value,
                             'S': shipping_costs[-1],
-                            'R': 'Shipping',
+                            'R': 'Shipping:SS',
                             'W': 'TRUE'})
 
         customer_row_nums.append(ws.max_row)
 
+        # Highlight added rows
         for row in customer_row_nums:
             for col in range(1, (ws.max_column + 1)):
                 char = get_column_letter(col)
-                ws[char + str(row)].fill = highlight
+                ws[char + str(row)].fill = highlight_green
 
     except ReadOnlyWorkbookException as e:
         print("Error: Trying to modify a read-only workbook!")
@@ -60,22 +80,22 @@ def main():
     excel_file = input("Provide excel file or filepath (ex. test.xlsx): ")
 
     try:
+        print("\n-> Loading Excel File...")
         wb = load_workbook(excel_file)
         ws = wb.active
-        print("Excel file successfully loaded!")
-        print("Modifying File...")
-        getCustomerInfo(ws)
+        formatOrderInfo(ws)
         insertRows(ws)
+        print("-> Modifying File...")
 
         try:
-            wb.save(excel_file)
-            print("Success!\n")
+            wb.save("MOD_" + excel_file)
+            print("*** Success! ***\n")
 
         except WorkbookAlreadySaved as e:
             print("Workbook Already Saved\n")
 
     except:
-        print("\nError: Failed to load Excel File! Make sure the filename/filepath is correct.")
+        print("\nERROR: Failed to load Excel File! Make sure the filename/filepath is correct.")
         print("Please re-run the script\n")
 
 
